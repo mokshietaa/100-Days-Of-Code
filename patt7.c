9b9ba2ac65c4928ceed47296e046914d73291aff
Mokshieta Ragta <mokshietaa@gmail.com>
2025-09-19 00:32:15 +0530

    Day-26 pattern 7

---
+++ patt7.c
@@ -0,0 +1,37 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+int main()
+{
+    int i, j, k, n = 4;
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i + 1; j++)
+        {
+            for (k = 1; k <= i; k++)
+            {
+                printf("*\n");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
