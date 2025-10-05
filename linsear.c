5d0db2438c5d6fa4c41276e8568be8a5fc10ed5d
Mokshieta Ragta <mokshietaa@gmail.com>
2025-10-05 10:55:33 +0530

    Day-31 linear search

---
+++ linsear.c
@@ -0,0 +1,25 @@
+//Q61: Search for an element in an array using linear search.
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, search, found = 0;
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &search);
+    for(i = 0; i < n; i++) {
+        if(arr[i] == search) {
+            found = 1;
+            break;
+        }
+    }
+    if(found) {
+        printf("Element %d found at index %d.\n", search, i);
+    } else {
+        printf("Element %d not found in the array.\n", search);
+    }
+    return 0;
+} 
